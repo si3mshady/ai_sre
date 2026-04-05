1b591198102685831764802b56b43f63ec51cc73
Ubuntu <si3mshady@Robot-Brain.twkf2fd54zwutfbc4bbtxdhkid.bx.internal.cloudapp.net>
2026-04-05 12:15:07 +0000

    added setup script

---
+++ master_launcher.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+PROFILE="gvisor-cluster"
+minikube delete --all
+minikube start --profile="$PROFILE" --driver=docker --container-runtime=containerd --cpus=4 --memory=8192
+minikube ssh --profile="$PROFILE" "curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash"
+minikube addons enable gvisor --profile="$PROFILE"
+
+helm install traefik traefik/traefik   --namespace traefik   --create-namespace   --version 37.3.0   -f traefik.yaml --skip-crds
+
+
+kubectl apply --server-side -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.4.0/experimental-install.yaml
+
+kubectl apply -n argocd --server-side --force-conflicts -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+
+helm repo add kyverno https://kyverno.github.io/kyverno/
+helm repo update
+helm install kyverno kyverno/kyverno -n kyverno --create-namespace
