12e8dc8cf1a2e407a4e35dcaddee035b619163ed
Ubuntu <si3mshady@Robot-Brain.twkf2fd54zwutfbc4bbtxdhkid.bx.internal.cloudapp.net>
2026-04-09 19:56:29 +0000

    added setup script

--- traefik/master_launcher.sh
+++ traefik/master_launcher.sh
@@ -4,9 +4,12 @@ PROFILE="gvisor-cluster"
 minikube delete --all
 minikube start --profile="$PROFILE" --driver=docker --container-runtime=containerd --cpus=4 --memory=8192
 minikube ssh --profile="$PROFILE" "curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash"
+
+sleep 30
+
 minikube addons enable gvisor --profile="$PROFILE"
 
-helm install traefik traefik/traefik   --namespace traefik   --create-namespace   --version 37.3.0   -f traefik.yaml --skip-crds
+helm install traefik traefik/traefik   --namespace traefik   --create-namespace   --version 37.3.0   -f ./traefik.yaml --skip-crds
 
 
 kubectl apply --server-side -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.4.0/experimental-install.yaml
